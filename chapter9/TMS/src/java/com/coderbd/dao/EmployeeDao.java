0cc5ad5a8c12e9e159d7715c3f09adc695f98d90
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-01-14 01:32:11 +0600

    primefaces and jsf and hibernate

---
+++ chapter9/TMS/src/java/com/coderbd/dao/EmployeeDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.coderbd.dao;
+
+import com.coderbd.common.CommonDao;
+import com.coderbd.entity.Employee;
+
+/**
+ *
+ * @author Rajaul Islam
+ */
+public interface EmployeeDao extends CommonDao<Employee>{
+    
+}
---
+++ chapter9/TMS/src/java/com/coderbd/entity/Student.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.coderbd.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Rajail Islam
+ */
+@Entity
+@Table(name = "student")
+public class Student implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "student_name")
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
