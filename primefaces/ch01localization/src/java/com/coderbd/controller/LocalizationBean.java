769bb2337f8b43608fb7b5ad384a61ec3bc680b4
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-01-21 18:56:06 +0600

    theme install example

---
+++ primefaces/ch01localization/src/java/com/coderbd/controller/LocalizationBean.java
@@ -0,0 +1,32 @@
+package com.coderbd.controller;
+
+import com.coderbd.utils.MessageUtil;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+/**
+ * User: mertcaliskan
+ * Date: 6/21/12
+ */
+@Named
+@ViewScoped
+public class LocalizationBean implements Serializable {
+
+	private boolean selectedValue = true;
+
+	public boolean isSelectedValue() {
+		return selectedValue;
+	}
+
+	public void setSelectedValue(boolean selectedValue) {
+		this.selectedValue = selectedValue;
+	}
+
+	public String addMessage() {
+		MessageUtil.addInfoMessage("broadcast.message");
+
+		return null;
+	}
+}
