f0fca738ac6ced45525516db492dea055c44139d
Mohammad Rajaul Islam <springapidev@gmail.com>
2019-01-03 00:04:05 +0600

    evidence crud

---
+++ jsfEvi/src/java/com/coderbd/entity/Student.java
@@ -0,0 +1,86 @@
+package com.coderbd.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String gender;
+    private String round[];
+    private String msg;
+    private Date dob;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String[] getRound() {
+        return round;
+    }
+
+    public void setRound(String[] round) {
+        this.round = round;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + ", email=" + email + ", gender=" + gender + ", round=" + round + ", msg=" + msg + ", dob=" + dob + '}';
+    }
+    
+    
+}
